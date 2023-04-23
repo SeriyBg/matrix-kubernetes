9cc8548ce4f4873b4b39b8b4910f9a140d7baea7
Serge Bishyr <seriybg@gmail.com>
2023-04-23 12:06:24 +0300

    change layout

--- kubernetes/apply_all.sh
+++ kubernetes/apply_all.sh
@@ -2,6 +2,6 @@
 
 kubectl apply -f architect.yaml
 kubectl apply -f agent.yaml
-kubectl apply -f oracle_configuration.yaml
+kubectl apply -f oracle.yaml
 kubectl apply -f merovingian.yaml
 kubectl apply -f twins.yaml
